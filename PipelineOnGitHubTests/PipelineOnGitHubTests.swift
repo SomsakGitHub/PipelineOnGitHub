d93d84589dcc1de07b135a6a8aa02922a84dafe3
somsak <somsak.kaeworasan@outlook.com>
2026-07-22 10:21:00 +0700

    init project.

---
+++ PipelineOnGitHubTests/PipelineOnGitHubTests.swift
@@ -0,0 +1,19 @@
+//
+//  PipelineOnGitHubTests.swift
+//  PipelineOnGitHubTests
+//
+//  Created by tiscomacnb2486 on 22/7/2569 BE.
+//
+
+import Testing
+@testable import PipelineOnGitHub
+
+struct PipelineOnGitHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
